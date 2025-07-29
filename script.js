a79808389e0384366140f9f5695f1a754c9e06f6
thalesluiz45 <thalesluiz45@hotmail.com>
2025-07-29 18:03:33 -0300

    simplified version

--- script.js
+++ script.js
@@ -19,71 +19,25 @@ modal.addEventListener("click", function (e) {
   }
 });
 
-function criarContainerImagem(src, salvar, index) {
-  var container = document.createElement("div");
-  container.className = "imagem-container";
-
-  var img = document.createElement("img");
-  img.src = src;
-  img.alt = "Imagem";
-
-  img.addEventListener("click", function () {
-    abrirModal(img.src);
-  });
-
-  var btnRemover = document.createElement("button");
-  btnRemover.textContent = "Remover";
-  btnRemover.className = "btn-remover";
-
-  btnRemover.addEventListener("click", function (e) {
-    e.stopPropagation();
-    container.remove();
-    if (index !== null) {
-      removerImagemDoLocalStoragePorIndice(index);
-      recarregarGaleria();
-    }
-  });
-
-  container.appendChild(img);
-  container.appendChild(btnRemover);
-  galeria.appendChild(container);
-
-  if (salvar !== false) {
-    salvarImagemNoLocalStorage(src);
-  }
-}
-
 uploadInput.addEventListener("change", function (event) {
   var file = event.target.files[0];
   if (!file) return;
 
   var reader = new FileReader();
   reader.onload = function (e) {
-    salvarImagemNoLocalStorage(e.target.result);
-    recarregarGaleria();
+    var container = document.createElement("div");
+    container.className = "imagem-container";
+
+    var img = document.createElement("img");
+    img.src = e.target.result;
+    img.alt = "Imagem";
+
+    img.addEventListener("click", function () {
+      abrirModal(img.src);
+    });
+
+    container.appendChild(img);
+    galeria.appendChild(container);
   };
   reader.readAsDataURL(file);
 });
-
-function salvarImagemNoLocalStorage(src) {
-  var imagens = JSON.parse(localStorage.getItem("galeriaImagens")) || [];
-  imagens.push(src);
-  localStorage.setItem("galeriaImagens", JSON.stringify(imagens));
-}
-
-function removerImagemDoLocalStoragePorIndice(index) {
-  var imagens = JSON.parse(localStorage.getItem("galeriaImagens")) || [];
-  imagens.splice(index, 1);
-  localStorage.setItem("galeriaImagens", JSON.stringify(imagens));
-}
-
-function recarregarGaleria() {
-  galeria.innerHTML = "";
-  var imagens = JSON.parse(localStorage.getItem("galeriaImagens")) || [];
-  for (var i = 0; i < imagens.length; i++) {
-    criarContainerImagem(imagens[i], false, i);
-  }
-}
-
-// Inicializa
-recarregarGaleria();
